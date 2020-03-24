d982a9b5ab932d390e7fe811fa0a18ce16d2ceae
Every1CanWriteACode <61137980+Every1CanWriteACode@users.noreply.github.com>
2020-03-24 15:13:37 +0200

    Initial colors

---
+++ js/actions.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// header
+
+// hero
+
+// about me
+
+// portfolio
+
+//  services
+
+//  team
+
+// numbers
+
+//  pricing
+
+//  blog
+
+// testimonials
+
+//  contact us
+
+//  maps
+
+// footer
