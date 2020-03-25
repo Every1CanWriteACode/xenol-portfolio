1aa9add34e69d11061f7749de07a8d247de4de5a
Every1CanWriteACode <61137980+Every1CanWriteACode@users.noreply.github.com>
2020-03-25 13:29:10 +0200

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,59 @@
+"use strict"
+// header
+
+// hero
+
+// about me
+
+// portfolio
+
+// services
+
+const services = [
+    {
+        icon: 'facebook',
+        title: 'Interface Design',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur explicabo et vel reiciendis culpa quidem id accusantium aliquam maxime!'
+    },
+    {
+        icon: 'twitter',
+        title: 'Experience Design',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur explicabo et vel reiciendis culpa quidem id accusantium aliquam maxime!'
+    },
+    {
+        icon: 'instagram',
+        title: 'Web Developmnent',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur explicabo et vel reiciendis culpa quidem id accusantium aliquam maxime!'
+    },
+    {
+        icon: 'linkedin',
+        title: 'Mobile App',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur explicabo et vel reiciendis culpa quidem id accusantium aliquam maxime!'
+    },
+    {
+        icon: 'facebook',
+        title: 'Digital Marketing',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur explicabo et vel reiciendis culpa quidem id accusantium aliquam maxime!'
+    },
+    {
+        icon: 'facebook',
+        title: 'Wooocommerce',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur explicabo et vel reiciendis culpa quidem id accusantium aliquam maxime!'
+    },
+    
+];
+
+
+// team
+
+// numbers
+
+// pricing
+
+// blog
+
+// contact us
+
+// maps
+
+// footer
